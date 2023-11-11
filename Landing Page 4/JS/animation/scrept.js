813a91418d6fea10f713fd2138d30349993772ec
Abhishek Gupta <abhishek9661342993@gmail.com>
2023-11-11 16:25:05 +0530

    update code

--- Landing Page 4/JS/animation/scrept.js
+++ Landing Page 4/JS/animation/scrept.js
@@ -1,10 +1,31 @@
 
+var cur = document.getElementById("cursor");
+var blur = document.querySelector("#blur");
+
+
+
 document.addEventListener('mousemove', function (dets) {
-    // console.log(dets)
-    console.log("fff")
+    console.log(dets.x)
+    cur.style.left = dets.x + "px";
+    cur.style.top = dets.y + "px";
+
+
+    blur.style.left = dets.x -  200 +"px";
+    blur.style.top = dets.y -200 + "px";
+    // blur.style.top = dets.y + "px";
+
+
+
+
+
+
+
+
 })
 
- 
+
+
+
 gsap.to("nav", {
     height: "10vh",
     backgroundColor: "black",
@@ -28,16 +49,49 @@ gsap.to("nav", {
 });
 
 gsap.to('main', {
-    backgroundColor: "black",
+    backgroundColor: "black", 
     scrollTrigger: {
         scroller: "body",
         trigger: "main",
-        markers: true,
-        scrub: 5,
+        // markers: true, 
+        scrub: 3,
 
-        start: "top -30%",
-        end: "top -40%",
+        start: "top -20%",
+        end: "top -30%",
     }
 })
 
 
+
+gsap.to('#page1 video', {
+  height:"120vh",
+    scrollTrigger: {
+        trigger: "#page1",
+        scroller: "body",
+        // markers:true, 
+        start: "top 20%",
+        end: "top 25%",
+        scrub: 1,
+        scale: 1,
+
+    }
+})
+
+gsap.to('#page2', {
+    backgroundColor: "black", 
+    y: -200,
+    duration: 1,
+    zIndex:5,
+    backgroundColor:"black",
+
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        markers:true, 
+        start: "top 20%",
+        end: "top 21%",
+        scrub: 2,
+        scale:2,
+        
+    }
+})
